7162e4cfbca0387d6e0b5a8d18a353bd86fbaeb0
Yolo-He <2684921675@qq.com>
2022-07-12 17:35:21 +0800

    拆分组件，根据时间显示课程

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,104 +1,28 @@
-import { useState } from 'react'
-import { Calendar, Button, Col, DatePicker, Drawer, Form, Input, Row, Space } from 'antd';
-import { PlusCircleOutlined } from '@ant-design/icons';
+import type { Moment } from 'moment';
+import { Calendar } from 'antd';
+import AddForm from "../components/AddForm";
 import './index.less';
+const data = require('../data.json');
+const item = data[0];
 
+const clickItem = (id: Number) => {
+  console.log(id)
+}
 
 const IndexPage = () => {
-  const [visible, setVisible] = useState(false);
-  const RangePicker: any = DatePicker.RangePicker;
-
-  const showDrawer = () => {
-    setVisible(true);
-  };
-  const onClose = () => {
-    setVisible(false);
+  const dateCellRender = (value: Moment) => {
+    return value.format('YYYY-MM-DD') === data[0].time.split(' ')[0] ? (
+      <span className='item' onClick={() => clickItem(item.c_id)}>
+        {item.c_id} {item.c_name}
+      </span >
+    ) : null;
   };
 
 
   return (
     <div className='c_width'>
-      <PlusCircleOutlined className='add_icon' onClick={showDrawer} />
-
-      <Calendar />
-
-      <Drawer
-        title="新增课程"
-        width={417}
-        onClose={onClose}
-        visible={visible}
-        extra={
-          <Space>
-            <Button onClick={onClose}>取消</Button>
-            <Button onClick={onClose} type="primary">提交</Button>
-          </Space>
-        }
-      >
-        <Form layout="vertical" hideRequiredMark>
-          <Col span={24}>
-            <Form.Item
-              name="c_id"
-              label="课程id"
-              rules={[{ required: true, message: '请输入课程id' }]}>
-              <Input placeholder="请输入课程id" />
-            </Form.Item>
-          </Col>
-          <Col span={24}>
-            <Form.Item
-              name="c_name"
-              label="课程名"
-              rules={[{ required: true, message: '请输入课程名' }]}>
-              <Input placeholder="请输入课程名" />
-            </Form.Item>
-          </Col>
-          <Col span={24}>
-            <Form.Item
-              name="time"
-              label="上课时间"
-              rules={[{ required: true, message: '请输入上课时间' }]}>
-              <RangePicker
-                showTime={{ format: 'HH:mm' }}
-                format="YYYY-MM-DD HH:mm"
-              />
-            </Form.Item>
-          </Col>
-          <Col span={24}>
-            <Form.Item
-              name="place"
-              label="上课地点"
-              rules={[{ required: true, message: '请输入上课地点' }]}
-            >
-              <Input placeholder="请输入上课地点" />
-            </Form.Item>
-          </Col>
-          <Row gutter={10}>
-            <Col span={12}>
-              <Form.Item
-                name="nm_money"
-                label="普通金额"
-                rules={[{ required: true, message: '请输入普通金额' }]}>
-                <Input placeholder="请输入普通金额" />
-              </Form.Item>
-            </Col>
-            <Col span={12}>
-              <Form.Item
-                name="na_money"
-                label="非预约金额"
-                rules={[{ required: true, message: '请输入非预约金额' }]}>
-                <Input placeholder="请输入非预约金额" />
-              </Form.Item>
-            </Col>
-          </Row>
-          <Col span={24}>
-            <Form.Item
-              name="p_limit"
-              label="课程人数"
-              rules={[{ required: true, message: '请输入课程人数' }]}>
-              <Input placeholder="请输入课程人数" />
-            </Form.Item>
-          </Col>
-        </Form>
-      </Drawer>
+      <AddForm />
+      <Calendar dateCellRender={dateCellRender} />
     </div >
   )
 }
