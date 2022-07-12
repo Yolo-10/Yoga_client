f1ae0eb76a46358104675054fa46374fd7dfbf7d
Yolo-He <2684921675@qq.com>
2022-07-12 15:39:24 +0800

    init

---
+++ .umirc.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'umi';
+
+export default defineConfig({
+  nodeModulesTransform: {
+    type: 'none',
+  },
+  routes: [
+    { path: '/', component: '@/pages/index' },
+  ],
+  fastRefresh: {},
+  locale: {
+    default: 'zh-CN', // 工程默认语言
+    antd: true,
+    // 默认为true。为true时，会使用`navigator.language`覆盖默认。为false时，则使用默认语言
+    baseNavigator: false,
+  },
+});
